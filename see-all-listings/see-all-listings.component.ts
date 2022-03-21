9322505a34a23412a71cb5d5ebd96d8468918a92
Elliot Culbertson <elliot@gmail.com>
2022-03-21 12:00:01 -0400

    added see-all-listings

---
+++ see-all-listings/see-all-listings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-all-listings',
+  templateUrl: './see-all-listings.component.html',
+  styleUrls: ['./see-all-listings.component.css']
+})
+export class SeeAllListingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
